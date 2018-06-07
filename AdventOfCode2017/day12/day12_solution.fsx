8d4835a7014c48a4a7095226c9e2a6fceec783fe
Gary Hubbard <hubbardgary@users.noreply.github.com>
2018-06-07 22:16:44 +0100

    Add solutions for days 12 and 13

---
+++ AdventOfCode2017/day12/day12_solution.fsx
@@ -0,0 +1,50 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let inputFilename = "day12_input.txt"
+let input = Path.Combine(__SOURCE_DIRECTORY__, inputFilename)
+
+let readInput (path:string) = File.ReadAllLines path
+
+let inputMappings = (readInput input) |> Array.toList
+
+let buildMap (mappings:string list):Map<int, int list> =
+    let rec addMap (mapping:string list, m:Map<int, int list>) =
+        match mapping with
+        | [] -> m
+        | x::xs -> let pattern = @"(?<pipe>\d+) <-> (?<conn>.*)"
+                   let matches = Regex.Match(x, pattern)
+                   let pipe = (matches.Groups.Item("pipe").Value) |> int
+                   let conns = matches.Groups.Item("conn").Value
+                   addMap (xs, (m |> Map.add pipe (conns.Split(',')
+                                  |> Array.map(fun c -> c.Trim())
+                                  |> Array.map int
+                                  |> Array.toList)))
+    addMap (mappings, Map.empty)
+
+let mappings = buildMap (inputMappings)
+
+let rec walkMap connections current = 
+    if Set.contains current connections then
+        connections
+    else
+        List.fold walkMap (connections.Add current) mappings.[current]
+
+let part1 = walkMap Set.empty 0 |> Set.count
+
+// Part 2
+let programIds = [0..1999]
+
+let rec countGroups knownGroups roots total =
+    match roots with
+    | [] -> total
+    | x::xs -> if knownGroups |> Set.contains x
+               then countGroups knownGroups xs total
+               else let group = walkMap Set.empty x
+                    countGroups (Set.union knownGroups group) xs (total + 1)
+
+let part2 = countGroups Set.empty programIds 0
+
+printfn "Part 1 = %i" part1
+printfn "Part 2 = %i" part2
+    
---
+++ AdventOfCode2017/day13/day13_solution.fsx
@@ -0,0 +1,38 @@
+﻿open System.IO
+
+let inputFilename = "day13_input.txt"
+let path = Path.Combine(__SOURCE_DIRECTORY__, inputFilename)
+
+let readInput (path:string) = File.ReadAllLines path
+let input = (readInput path) |> Array.toList
+
+let firewallMap input =
+    let rec addLayer (layers, firewall:Map<int, int>) =
+        match layers:string list with
+        | [] -> firewall
+        | x::xs -> let layer = x.Split(':')
+                               |> Array.map (fun x -> x.Trim())
+                               |> Array.map int
+                   addLayer (xs, (firewall |> Map.add layer.[0] layer.[1]))
+    addLayer (input, Map.empty)
+        
+// Part 1
+let firewall = firewallMap input
+let calculateSeverity f = f |> Map.filter (fun k v -> k = 0 || v < 2 || (k % (v * 2 - 2) = 0))
+                            |> Map.fold (fun acc k v -> k * v + acc) 0
+
+let part1 = calculateSeverity firewall
+printfn "Part 1 = %i" part1
+
+// Part 2
+let delayByOnePicosecond map:Map<int, int> =
+    map |> Map.fold (fun m k v -> m.Add(k + 1, v)) Map.empty
+
+let rec solvePart2 firewall =
+    let severity = calculateSeverity firewall
+    if severity = 0 then firewall
+    else solvePart2 (delayByOnePicosecond firewall)
+
+let zeroSeverityMap = solvePart2 firewall
+let part2 = zeroSeverityMap |> Map.toSeq |> Seq.head |> fst
+printfn "Part 2 = %i" part2
