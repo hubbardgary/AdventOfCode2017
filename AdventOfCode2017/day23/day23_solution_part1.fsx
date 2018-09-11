eb72e452016ee2bc71dc20ea4b1869531d8ed074
Gary Hubbard <hubbardgary@users.noreply.github.com>
2018-09-11 23:03:06 +0100

    Day 23 solution

---
+++ AdventOfCode2017/day23/day23_solution_part1.fsx
@@ -0,0 +1,64 @@
+﻿open System
+open System.IO
+open System.Collections.Generic
+
+let inputFilename = "day23_input.txt"
+let path = Path.Combine(__SOURCE_DIRECTORY__, inputFilename)
+
+let readInput (path:string) = File.ReadAllLines path
+
+let registers = new Dictionary<string, int64>(dict [ ("a", 0L); ("b", 0L); ("c", 0L); ("d", 0L); ("e", 0L); ("f", 0L); ("g", 0L); ("h", 0L) ])
+
+let mutable nextInstruction = 0
+
+let incrementInstruction i = nextInstruction <- nextInstruction + i
+
+let parseInt64 str =
+    match Int64.TryParse(str) with
+    | (true, int64) -> Some(int64)
+    | _ -> None
+
+let set reg x =
+    let i = parseInt64 x
+    match i with
+    | None -> registers.[reg] <- registers.[x]
+    | Some(i) -> registers.[reg] <- i
+    incrementInstruction 1
+
+let updateRegister reg x op =
+    let i = parseInt64 x
+    match i with
+    | None -> registers.[reg] <- (op registers.[reg] registers.[x])
+    | Some(i) -> registers.[reg] <- (op registers.[reg] i)
+    incrementInstruction 1
+
+let sub reg x = updateRegister reg x (-)
+let mul reg x = updateRegister reg x (*)
+    
+let jnz testvalue jump =
+    let isInt = parseInt64 testvalue
+    match isInt with
+    | None -> if registers.[testvalue] <> 0L
+              then let a = parseInt64 jump
+                   let offset = (if a.IsNone then registers.[jump] else a.Value) |> int
+                   incrementInstruction offset
+              else incrementInstruction 1
+    | Some(isInt) -> if isInt <> 0L then incrementInstruction (jump |> int) else incrementInstruction 1
+
+let part1 =
+    let instructions = readInput path
+    let rec execute i mulCalls =
+        if i < (instructions |> Array.length) then
+            let parts = instructions.[nextInstruction].Split()
+            match parts.[0] with
+            | "set" -> set parts.[1] parts.[2]
+            | "sub" -> sub parts.[1] parts.[2]
+            | "mul" -> mul parts.[1] parts.[2]
+            | "jnz" -> jnz parts.[1] parts.[2]
+            | _ -> ()
+            execute nextInstruction (if parts.[0] = "mul" then mulCalls + 1 else mulCalls)
+        else
+            mulCalls
+    execute nextInstruction 0
+
+printfn "Part 1 = %i" part1
---
+++ AdventOfCode2017/day23/day23_solution_part2.fsx
@@ -0,0 +1,28 @@
+﻿let mutable a = 1
+let mutable b = 0
+let mutable c = 0
+let mutable d = 0
+let mutable h = 0
+
+b <- 81
+c <- b
+
+if a = 1 then
+    b <- b * 100
+    b <- b + 100000
+    c <- b + 17000
+
+while b <= c do
+    d <- 2
+    let mutable breakLoop = false
+    
+    // If b is divisible by d with no remainder (i.e. it is not prime), increment h
+    // h is counting non-prime numbers between b and c inclusive, increasing b by 17 after each iteration.
+    while d < b && not breakLoop do
+        if b % d = 0 then h <- h + 1
+                          breakLoop <- true
+        d <- d + 1
+    
+    b <- b + 17
+
+printfn "Part 2 = %i" h
